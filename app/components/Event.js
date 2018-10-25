3f82d43462f4fd110e6951969024949ee0d12959
Sina Kamali Anaraki <>
2018-10-24 22:22:21 -0400

    Event page completed

--- app/components/Event.js
+++ app/components/Event.js
@@ -1,26 +1,20 @@
-import React, {Component} from 'react';
-import {AppRegistry,Platform, StyleSheet, Text, View, ImageBackground, BackHandler, Image,TouchableOpacity, Button, TextInput, ScrollView,CheckBox,} from 'react-native';
-import {createStackNavigator} from 'react-navigation'
+import React, { Component } from 'react';
+import { AppRegistry, Platform, StyleSheet, Text, View, ImageBackground, BackHandler, Image, TouchableOpacity, Button, TextInput, ScrollView, CheckBox, } from 'react-native';
+import { createStackNavigator } from 'react-navigation'
 import Slideshow from 'react-native-slideshow';
 import PropTypes from 'prop-types';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 
 export default class Event extends Component {
 
-  // constructor(){
-  //   super();
-  //   this.state ={
-  //     check:false
-  //   }
-	// //placeholder to fetch slides from event object
-  //   this.slidePictures = ['http://placeimg.com/640/480/any'];
-	// this.slideInformation = 
-	// 'Cineplex Inc. is a Canadian entertainment company headquartered in Toronto, Ontario. Through its operating subsidiary Cineplex Entertainment LP, Cineplex operates 162 theatres across Canada.';
-  // }
 
   constructor(props) {
     super(props);
 
     this.state = {
+      myText: 'I\'m ready to get swiped!',
+      gestureName: 'none',
+      backgroundColor: '#fff',
       position: 1,
       interval: null,
       dataSource: [
@@ -55,15 +49,53 @@ export default class Event extends Component {
     clearInterval(this.state.interval);
   }
 
-  static navigationOptions = ({navigation, screenProps}) => ({
+  onSwipeUp(gestureState) {
+    this.setState({ myText: 'You swiped up!' });
+  }
+
+  onSwipeDown(gestureState) {
+    this.setState({ myText: 'You swiped down!' });
+  }
+
+  onSwipeLeft(gestureState) {
+    this.setState({ myText: 'You swiped left!' });
+  }
+
+  onSwipeRight(gestureState) {
+    this.setState({ myText: 'You swiped right!' });
+  }
+
+  onSwipe(gestureName, gestureState) {
+    const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+    this.setState({ gestureName: gestureName });
+    switch (gestureName) {
+      case SWIPE_UP:
+        this.setState({ backgroundColor: 'red' });
+        break;
+      case SWIPE_DOWN:
+        this.setState({ backgroundColor: 'green' });
+        break;
+      case SWIPE_LEFT:
+        this.setState({ backgroundColor: 'blue' });
+        break;
+      case SWIPE_RIGHT:
+        this.setState({ backgroundColor: 'yellow' });
+        break;
+    }
+  }
+
+
+
+
+  static navigationOptions = ({ navigation, screenProps }) => ({
     headerTitle: (
-      <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
         <Image
-            source={require('../images/logo.png')}
-            style={{width:80, height:80}}
+          source={require('../images/logo.png')}
+          style={{ width: 80, height: 80 }}
         />
       </View>
-          ),
+    ),
     headerStyle: {
       backgroundColor: '#f4511e',
       height: 90
@@ -73,23 +105,23 @@ export default class Event extends Component {
       fontWeight: 'bold',
     },
     headerRight: (
-      <TouchableOpacity style={{textAlign:'center', marginRight: 10}}
-          onPress={() => navigation.navigate('MyFriends')}>
-          <Image
-            source={require('../images/Messages.png')}
-            style={{width:40, height:40}}
+      <TouchableOpacity style={{ textAlign: 'center', marginRight: 10 }}
+        onPress={() => navigation.navigate('MyFriends')}>
+        <Image
+          source={require('../images/Messages.png')}
+          style={{ width: 40, height: 40 }}
         />
       </TouchableOpacity>
-      ),
-      headerLeft: (
-        <TouchableOpacity style={{textAlign:'center', marginLeft: 10}}
-          onPress={() => navigation.navigate('Preference')}>
-          <Image
-            source={require('../images/Settings.png')}
-            style={{width:40, height:40}}
+    ),
+    headerLeft: (
+      <TouchableOpacity style={{ textAlign: 'center', marginLeft: 10 }}
+        onPress={() => navigation.navigate('Preference')}>
+        <Image
+          source={require('../images/Settings.png')}
+          style={{ width: 40, height: 40 }}
         />
-        </TouchableOpacity>
-        )  
+      </TouchableOpacity>
+    )
   });
 
   componentDidMount() {
@@ -97,52 +129,66 @@ export default class Event extends Component {
   }
 
   componentWillUnmount() {
-      BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressed);
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressed);
   }
 
   onBackButtonPressed() {
-      return true;
+    return true;
   }
 
   render() {
+
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
     return (
-        <ImageBackground source={require('../images/background.png')} style={{width: '100%', height: '100%'}}>
-          <ScrollView>
-            <View style={styles.container}>
+      <ImageBackground source={require('../images/background.png')} style={{ width: '100%', height: '100%' }}>
+        <ScrollView>
+          <View style={[styles.container],{paddingTop: 20}}>
+            <GestureRecognizer
+              onSwipe={(direction, state) => this.onSwipe(direction, state)}
+              onSwipeUp={(state) => this.onSwipeUp(state)}
+              onSwipeDown={(state) => this.onSwipeDown(state)}
+              onSwipeLeft={(state) => this.onSwipeLeft(state)}
+              onSwipeRight={(state) => this.onSwipeRight(state)}
+              config={config}
+              style={{
+                flex: 1,
+                backgroundColor: this.state.backgroundColor
+              }}
+            >
+              <View style={{ backgroundColor: 'white', padding: 30, color: 'black', textAlign: 'center',}}>
+                <Slideshow
+                  dataSource={this.state.dataSource}
+                  position={this.state.position}
+                  onPositionChanged={position => this.setState({ position })} />
 
- <Slideshow 
-        dataSource={this.state.dataSource}
-        position={this.state.position}
-        onPositionChanged={position => this.setState({ position })} />
+                <Text style={{ marginTop: 20 }}>{this.state.myText}</Text>
+                <Text>onSwipe callback received gesture: {this.state.gestureName}</Text>
+                <View style={{ marginTop: 30, justifyContent:'space-between', flexDirection: 'row',}}>
+                  <TouchableOpacity
+                  onPress={(state) => this.onSwipeLeft(state)}
+                  >
+                    <Image
+                      source={require('../images/no.png')}
+                    />
+                  </TouchableOpacity>
 
-			{/* <Text>
-				{this.slideInformation}
-			</Text>
-			<TouchableOpacity style={{marginTop:20}}
-                onPress={() => this.props.navigation.navigate('CreateEvent')}>
-                    <Text style = {styles.buttons}>
-                    Create Event
-                    </Text>
-            </TouchableOpacity>
-			<TouchableOpacity style={{marginTop:10}}
-                onPress={() => this.props.navigation.navigate('Event')}>
-                    <Text style = {styles.buttons}>
-                    Swipe Right
-                    </Text>
-            </TouchableOpacity>
-			<TouchableOpacity style={{marginTop:10}}
-                onPress={() => this.props.navigation.navigate('Event')}>
-                    <Text style = {styles.buttons}>
-                    Swipe Left
-                    </Text>
-            </TouchableOpacity>
-            
- */}
-
-            
-            </View>
-          </ScrollView>
-        </ImageBackground>
+                  <TouchableOpacity
+                  onPress={(state) => this.onSwipeRight(state)}
+                  >
+                    <Image
+                      source={require('../images/yes.png')}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </GestureRecognizer>
+          </View>
+        </ScrollView>
+      </ImageBackground>
 
     );
   }
@@ -150,30 +196,30 @@ export default class Event extends Component {
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    logo: {
-        width: 200,
-        height: 200,
-    },
-    text: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    buttons: {
-        borderWidth: 2,
-        padding: 10,
-        borderColor: 'white',
-        width: 330,
-        textAlign: "center",
-        fontSize: 20,
-        color: 'white',
-        fontWeight:'bold'
-     }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  buttons: {
+    borderWidth: 2,
+    padding: 10,
+    borderColor: 'white',
+    width: 330,
+    textAlign: "center",
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold'
+  }
 
-  });
-  AppRegistry.registerComponent(Event, () => Event);
+});
+AppRegistry.registerComponent(Event, () => Event);
